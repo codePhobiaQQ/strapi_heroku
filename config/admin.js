f3b1b399b8e712514866f6fd7232ca2f60b57728
vitalik-fan <63237530+vitalik-fan@users.noreply.github.com>
2022-02-10 02:37:42 +0300

    change_stari

--- config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '6b6ed3e06b6db0fe440f9e8313e65397'),
-  },
-});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47d8478b-ccf5-4b33-a451-c84d38efdeae'
+};
